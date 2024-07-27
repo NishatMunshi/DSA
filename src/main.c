197b6ee1dcca69165077cc459779acd5a901a89e
Nishat Munshi <nishatmunshi9@gmail.com>
2024-07-28 02:44:48 +0530

    added list

--- src/main.c
+++ src/main.c
@@ -5,13 +5,15 @@
 #include <stdio.h>
 
 #define ARRAY_LEN(array) (sizeof(array) / sizeof(array[0]))
+
 int main() {
     int array[] = { 3, 6, 7, 5, 4, 9, 1 };
     tree_sort(array, ARRAY_LEN(array));
+
     list* list = list_create();
     for (size_t i = 0; i < ARRAY_LEN(array); ++i) {
         list_add_before(list, 0, array[i]);
     }
-    printf("\n");
+
     return 0;
 }
