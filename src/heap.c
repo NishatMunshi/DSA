01f2b5124651e8810298fb0e1806af2b54f00029
Nishat Munshi <nishatmunshi9@gmail.com>
2024-07-28 12:38:54 +0530

    added compare function pointer to heap

--- src/heap.c
+++ src/heap.c
@@ -23,7 +23,7 @@ heap *heap_create(size_t capacity, int (*compare)(int x, int y)) {
 }
 
 #define PARENT(child) ((child - 1) / 2)
-#define COMAPARE(a, b) ((heap->compare(heap->items[a], heap->items[b]) > 0) ? (a) : (b))
+#define COMPARE(a, b) ((heap->compare(heap->items[a], heap->items[b]) > 0) ? (a) : (b))
 
 static void heap_swap(int *a, int *b) {
     int c = *a;
@@ -39,16 +39,16 @@ void heap_insert(heap *heap, int data) {
 
     heap->items[heap->size++] = data;
 
-    for (size_t child = heap->size - 1, parent = PARENT(child), min = COMAPARE(parent, child);
+    for (size_t child = heap->size - 1, parent = PARENT(child), min = COMPARE(parent, child);
          child > 0 && min != parent;
-         child = parent, parent = PARENT(child), min = COMAPARE(parent, child)) {
+         child = parent, parent = PARENT(child), min = COMPARE(parent, child)) {
         heap_swap(heap->items + parent, heap->items + min);
     }
 }
 
 #define L_CHILD(parent) ((parent) * 2 + 1)
 #define R_CHILD(parent) ((parent) * 2 + 2)
-#define COMPARE3(a, b, c) (COMAPARE((a), COMAPARE((b), (c))))
+#define COMPARE3(a, b, c) (COMPARE((a), COMPARE((b), (c))))
 
 int heap_remove(heap *heap) {
     if (heap->size == 0) {
