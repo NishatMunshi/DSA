e2937e53d289131ad2031debb696a6f5942e8a50
Nishat Munshi <nishatmunshi9@gmail.com>
2024-07-30 09:08:11 +0530

    done for now

--- src/main.c
+++ src/main.c
@@ -1,26 +1,8 @@
 #include "../include/main.h"
-#include "../include/heap.h"
+
 #include <stdio.h>
 
-int compare(int x, int y)
-{
-    return y - x;
-}
 int main() {
-    heap* heap = heap_create(10, compare);
-    heap_insert(heap, 1);
-    heap_insert(heap, 3);
-    heap_insert(heap, 2);
-    heap_insert(heap, 5);
-    heap_insert(heap, 6);
-    heap_insert(heap, 4);
-
-
-    printf("%i ", heap_remove(heap));
-    printf("%i ", heap_remove(heap));
-    printf("%i ", heap_remove(heap));
-    printf("%i ", heap_remove(heap));
-    printf("%i ", heap_remove(heap));
-    printf("%i ", heap_remove(heap));
+    printf("HELLO\n");
     return 0;
 }
